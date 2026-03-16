335f7c228cae53f9600adb10f8fbf3b5d7314bbe
Leandro <leandro0rodriguea@gmail.com>
2026-03-16 16:41:28 -0300

    Resolução completa da questão 3 da primeira lista

--- src/main/java/br/com/leandro/idt/Lista1_3.java
+++ src/main/java/br/com/leandro/idt/Lista1_3.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 
 public class Lista1_3 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in); LimparTela();
-        
+        Scanner s = new Scanner(System.in);
+        int valor = 0;
+        int quantidade = 0;
         while (true) {
-            int valor = 0;
-            int quantidade = 0;
-
+            LimparTela();
             System.out.print("Digite um valor a ser calculado: ");
             int nValor = s.nextInt(); valor += nValor; quantidade += 1;
 
@@ -17,8 +16,15 @@ public class Lista1_3 {
 
             if (opt == 1) { continue; }
             else if (opt == 2) { break; }
- 
         }
+        float media = valor / quantidade;
+        LimparTela(); s.nextLine();
+        if (media < 501) { System.out.printf("Seu saldo médio foi de %.2f, não foi possível liberar crédito.\n\nPressione ENTER para encerrar...", media); }
+        else if(media >= 501 && media < 1001) { System.out.printf("Seu saldo médio foi de %.2f, o crédito liberado será de 30%% do saldo médio.\n\nPressione ENTER para encerrar...", media); }
+        else if(media >= 1001 && media < 3001) { System.out.printf("Seu saldo médio foi de %.2f, o crédito liberado será de 40%% do saldo médio.\n\nPressione ENTER para encerrar...", media); }
+        else if(media >= 3001) { System.out.printf("Seu saldo médio foi de %.2f, o crédito liberado será de 50%% do saldo médio.\n\nPressione ENTER para encerrar...", media); }
+        s.nextLine(); LimparTela(); s.close();
+
     } public static void LimparTela() {
         System.out.print("\033[H\033[2J");
     }

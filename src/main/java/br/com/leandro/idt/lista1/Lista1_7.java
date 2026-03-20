e66f507eb76a7870fd3a2dbc374d5c1e075ce547
Leandro <leandro0rodriguea@gmail.com>
2026-03-20 13:16:57 -0300

    Atualização da estrutura das questões 6, 7, 8, 9 e 10 da lista 1

--- src/main/java/br/com/leandro/idt/lista1/Lista1_7.java
+++ src/main/java/br/com/leandro/idt/lista1/Lista1_7.java
@@ -3,14 +3,24 @@ import java.util.Scanner;
 
 public class Lista1_7 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in); LimparTela();
+        Scanner s = new Scanner(System.in); 
+        LimparTela();
 
-        System.out.print("Escolha um número: "); int num = s.nextInt(); s.nextLine();
+        System.out.print("Escolha um número: "); 
+        int num = s.nextInt(); 
+        s.nextLine();
 
-        if (num %2 == 0) {System.out.print("\nSeu número é par");}
-        else if (num %2 > 0) {System.out.print("\nSeu número é impar");}
+        if (num %2 == 0) {
+            System.out.print("\nSeu número é par");
+        }
+        else if (num %2 > 0) {
+            System.out.print("\nSeu número é impar");
+        }
 
-        System.out.print("\n\nPressione ENTER para encerrar..."); s.nextLine(); s.close(); LimparTela();
+        System.out.print("\n\nPressione ENTER para encerrar..."); 
+        s.nextLine(); 
+        s.close(); 
+        LimparTela();
 
     } public static void LimparTela() {System.out.print("\033[H\033[2J");}
 }
--- src/main/java/br/com/leandro/idt/lista1/Lista1_9.java
+++ src/main/java/br/com/leandro/idt/lista1/Lista1_9.java
@@ -3,24 +3,45 @@ import java.util.Scanner;
 
 public class Lista1_9 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in); LimparTela();
+        Scanner s = new Scanner(System.in); 
+        LimparTela();
 
-        System.out.print("Informe o primeiro valor: "); int v1 = s.nextInt(); LimparTela();
-        System.out.print("Informe o segundo valor: "); int v2 = s.nextInt(); LimparTela();
-        System.out.print("Informe o terceiro valor: "); int v3 = s.nextInt(); LimparTela();
-        System.out.print("Informe o quarto valor: "); int v4 = s.nextInt(); LimparTela();
-        System.out.print("Informe o quinto valor: "); int v5 = s.nextInt(); LimparTela();
+        System.out.print("Informe o primeiro valor: "); 
+        int v1 = s.nextInt(); 
+        LimparTela();
+        System.out.print("Informe o segundo valor: "); 
+        int v2 = s.nextInt(); 
+        LimparTela();
+        System.out.print("Informe o terceiro valor: "); 
+        int v3 = s.nextInt(); 
+        LimparTela();
+        System.out.print("Informe o quarto valor: "); 
+        int v4 = s.nextInt(); 
+        LimparTela();
+        System.out.print("Informe o quinto valor: "); 
+        int v5 = s.nextInt(); 
+        LimparTela();
 
         int media = (v1+v2+v3+v4+v5) / 5;
         StringBuilder todos = new StringBuilder();
         StringBuilder res = new StringBuilder();
         todos.append(String.format("[%d] [%d] [%d] [%d] [%d]", v1, v2, v3, v4, v5));
         
-        if (v1 > media) {res.append(String.format("[%d] ", v1));}
-        if (v2 > media) {res.append(String.format("[%d] ", v2));}
-        if (v3 > media) {res.append(String.format("[%d] ", v3));}
-        if (v4 > media) {res.append(String.format("[%d] ", v4));}
-        if (v5 > media) {res.append(String.format("[%d] ", v5));}
+        if (v1 > media) {
+            res.append(String.format("[%d] ", v1));
+        }
+        if (v2 > media) {
+            res.append(String.format("[%d] ", v2));
+        }
+        if (v3 > media) {
+            res.append(String.format("[%d] ", v3));
+        }
+        if (v4 > media) {
+            res.append(String.format("[%d] ", v4));
+        }
+        if (v5 > media) {
+            res.append(String.format("[%d] ", v5));
+        }
 
         System.out.printf("A media foi de: %d\n\nOs valores indicados foram: %s\n\nOs valores acima da média são: %s\n\n", media, todos, res);
         s.close();

90a0b3a19b6b5e0d326f157e783308a3dbc4781a
Leandro <leandro0rodriguea@gmail.com>
2026-06-18 14:44:40 -0300

    (Incompleto) Resolução da segunda questão da terceira lista de subprogramas.

---
+++ src/main/java/br/com/leandro/idt/Subprogramas_3/Q2.java
@@ -0,0 +1,50 @@
+package br.com.leandro.idt.Subprogramas_3;
+import java.util.Scanner;
+
+public class Q2 {
+    static Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        while (true) {
+            limparTela();
+
+            System.out.print("Escolha um valor pra checar se o mesmo é um número perfeito: ");
+            int val = s.nextInt();
+            limparTela();
+
+            if (val <= 1) {
+                System.out.print("O valor dado é inválido, tente novamente!\n\n");
+                System.exit(0);
+            }
+            String resultado = checarPerfeito(val);
+            if (resultado.length() == 0) {
+                System.out.print("O valor dado não é perfeito, pois a soma de seus divisores, exceto ele mesmo, não é igual a ele!\n\n");
+            }
+            else {
+                System.out.printf("O valor informado é perfeito, pois a soma de seus divisores (exceto ele) resulta nele mesmo! Os seus divisores são:\n\n%s\n", resultado);
+            }
+
+            System.out.print("Deseja checar outro valor(S/N)?: ");
+            String continuar = s.next();
+            if (continuar.equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+    }
+    private static String checarPerfeito(int val) {
+        int somaDivisores = 0;
+        String divisores = "";
+        for (int i = 1; i < val; i++) {
+            if (val % i == 0) {
+                somaDivisores += i;
+                divisores += String.format("%d\n", i);
+            }
+        }
+        if (somaDivisores != val) {
+            divisores = "";
+        }
+        return(divisores);
+    }
+    public static void limparTela() {
+        System.out.print("\033[H\033[2J");
+    }
+}

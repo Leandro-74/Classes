5219823e1ea634adad1d694c5c91fa282edfe501
Leandro <leandro0rodriguea@gmail.com>
2026-06-30 13:07:17 -0300

    Resolução da segunda questão da terceira lista de subprogramas.

--- src/main/java/br/com/leandro/idt/Subprogramas_3/Q2.java
+++ src/main/java/br/com/leandro/idt/Subprogramas_3/Q2.java
@@ -6,6 +6,9 @@ public class Q2 {
     public static void main(String[] args) {
         String listaPerfeitos = "";
         String listaImperfeitos = "";
+        int maiorPerfeito = 0;
+        int menorImperfeito = 0;
+
         while (true) {
             limparTela();
 
@@ -21,12 +24,12 @@ public class Q2 {
             if (resultado.length() == 0) {
                 System.out.print("O valor dado não é perfeito, pois a soma de seus divisores, exceto ele mesmo, não é igual a ele!\n\n");
                 listaImperfeitos = lista(val, listaImperfeitos);
-                int menorImperfeito = calcMenor(val);
+                menorImperfeito = calcMenor(val, menorImperfeito);
             }
             else {
                 System.out.printf("O valor informado é perfeito, pois a soma de seus divisores (exceto ele) resulta nele mesmo! Os seus divisores são:\n\n%s\n", resultado);
                 listaPerfeitos = lista(val, listaPerfeitos);
-                int maiorPerfeito = calcMenor(val);
+                maiorPerfeito = calcMaior(val, maiorPerfeito);
             }
 
             System.out.print("Deseja checar outro valor(S/N)?: ");
@@ -37,10 +40,17 @@ public class Q2 {
         }
         limparTela();
         if (listaPerfeitos.length() > 0) {
-            System.out.printf("Os números perfeitos dados foram:\n%s\n\n", listaPerfeitos);
+            System.out.printf("Houveram números perfeitos, sendo o maior deles %d! Segue a lista de números perfeitos:\n%s\n\n", maiorPerfeito, listaPerfeitos);
         }
         else {
-            System.out.print("Não houve nenhum número perfeito listado!\n\n");
+            System.out.print("Não houve nenhum número perfeito informado!\n\n");
+        }
+
+        if (listaImperfeitos.length() > 0) {
+            System.out.printf("Houveram números imperfeitos, sendo o menor deles %d! Segue a lista de números imperfeitos:\n%s\n\n", menorImperfeito, listaImperfeitos);
+        }
+        else {
+            System.out.print("Não houve nenhum número imperfeito informado!");
         }
     }
     public static String checarPerfeito(int val) {
@@ -61,11 +71,27 @@ public class Q2 {
         lista += String.format("\n%d", valor);
         return(lista);
     }
-    public static int calcMaior(int valor) {
-        if (valor = 0)
+    public static int calcMaior(int perfeito, int maior) {
+        if (maior == 0) {
+            maior = perfeito;
+        }
+        else {
+            if (perfeito > maior) {
+                maior = perfeito;
+            }
+        }
+        return(maior);
     }
-    public static int calcMenor(int valor) {
-
+    public static int calcMenor(int imperfeito, int menor) {
+        if (menor == 0) {
+            menor = imperfeito;
+        }
+        else {
+            if (imperfeito < menor) {
+                menor = imperfeito;
+            }
+        }
+        return(menor);
     }
     public static void limparTela() {
         System.out.print("\033[H\033[2J");

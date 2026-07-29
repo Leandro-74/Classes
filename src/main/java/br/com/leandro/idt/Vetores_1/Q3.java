db6924223474326c7c0e346cb7fd769b23b17314
Leandro <leandro0rodriguea@gmail.com>
2026-07-29 11:50:58 -0300

    Correção da terceira questão da primeira lista de vetores: retira o retorno de funções que alteram o vetor corrige estrutura de código

--- src/main/java/br/com/leandro/idt/Vetores_1/Q3.java
+++ src/main/java/br/com/leandro/idt/Vetores_1/Q3.java
@@ -36,7 +36,7 @@ public class Q3 {
             System.out.printf("[%d] ", vetor[i]);
         }
 
-        vetor = inverterVetor(vetor);
+        inverterVetor(vetor);
         System.out.print("\n\nVetor invertido: ");
         for (int i = 0; i < vetor.length; i++) {
             System.out.printf("[%d] ", vetor[i]);
@@ -44,34 +44,30 @@ public class Q3 {
         s.close();
 
     }
-    public static int[] preencherRandom(int[] vetor) {
+    public static void preencherRandom(int[] vetor) {
         Random r = new Random();
         for (int i = 0; i < vetor.length; i++) {
             vetor[i] = r.nextInt(100);
         }
-        return(vetor);
     }
-    public static int[] preencherSequencial(int[] vetor) {
-        for (int i = 1; i <= vetor.length; i++) {
-            vetor[i-1] = i;
+    public static void preencherSequencial(int[] vetor) {
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = i+1;
         }
-        return(vetor);
     }
-    public static int[] preencherManual(int [] vetor, Scanner s) {
+    public static void preencherManual(int [] vetor, Scanner s) {
         limparTela();
-        for (int i = 1; i <= vetor.length; i++) {
-            System.out.printf("Qual o valor para a %dª posição do vetor?: ", i);
-            vetor[i-1] = s.nextInt();
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.printf("Qual o valor para a %dª posição do vetor?: ", i+1);
+            vetor[i] = s.nextInt();
         }
-        return(vetor);
     }
-    public static int[] inverterVetor(int[] vetorOriginal) {
-        for (int i = 0; i < vetorOriginal.length/2; i++) {
-            int aux = vetorOriginal[i];
-            vetorOriginal[i] = vetorOriginal[i+8];
-            vetorOriginal[i+8] = aux;
+    public static void inverterVetor(int[] vetor) {
+        for (int i = 0; i < vetor.length/2; i++) {
+            int aux = vetor[i];
+            vetor[i] = vetor[i+(vetor.length/2)];
+            vetor[i+(vetor.length/2)] = aux;
         }
-        return(vetorOriginal);
     }
     public static void limparTela() {
         System.out.print("\033[H\033[2J");

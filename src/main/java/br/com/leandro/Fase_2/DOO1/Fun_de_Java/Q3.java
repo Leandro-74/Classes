2a71f9d4a236b894df0864de6969bec480dc93cd
Leandro <leandro0rodriguea@gmail.com>
2026-08-17 09:28:53 -0300

    Resolução da terceira questão da lista de Fundamentos de Java

---
+++ src/main/java/br/com/leandro/Fase_2/DOO1/Fun_de_Java/Q3.java
@@ -0,0 +1,18 @@
+package br.com.leandro.Fase_2.DOO1.Fun_de_Java;
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    int N = s.nextInt();
+    long result = 1;
+
+    if (N >= 1) {
+        for (int i = N; i > 0; i--) {
+            result *= i;
+        }
+    }
+    System.out.print(result);
+}   
+}
